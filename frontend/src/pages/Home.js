b787bfcde4c0f78bab9181cb9a678c674422d47d
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-02-10 20:59:33 -0500

    Add custom home page details

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
 
-export default function HomePage() {
+export default function Homepage() {
+  const { loading, error, data } = useFetch(
+    "http://localhost:1337/api/reviews"
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
+
   return (
     <div>
-      Home Page
-      <p>this is a paragraph</p>
+      {data.data.map((review) => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+
+          <small>console list</small>
+
+          <p>{review.attributes.body.substring(0, 200)}...</p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
     </div>
   );
 }
