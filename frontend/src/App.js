8a65a0c90049295edbfdc4fa801e0e9110e2c471
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-02-10 19:42:44 -0500

    Add main app return html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,22 +8,22 @@ import SiteHeader from "./components/SiteHeader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/details/:id">
+            <ReviewDetailsPage />
+          </Route>
+          <Route path="/category/:id">
+            <CategoryPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
