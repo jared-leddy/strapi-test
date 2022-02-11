f9fe6231d41d6b5c4d157c57253f1aa28d630444
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-02-10 22:50:50 -0500

    Update ReviewDetails.js

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
-export default function ReviewDetailsPage() {
+export default function ReviewDetails() {
+  const { id } = useParams();
+  const { loading, error, data } = useFetch(
+    `http://localhost:1337/api/reviews/${id}`
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
+
   return (
-    <div>
-      Review Details Page
-      <p>this is a paragraph</p>
+    <div className="review-card">
+      <div className="rating">{data.data.attributes.rating}</div>
+      <h2>{data.data.attributes.title}</h2>
+
+      <small>console list</small>
+
+      <p>{data.data.attributes.body}</p>
     </div>
   );
 }
